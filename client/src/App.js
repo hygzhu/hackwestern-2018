26b3af95cff230e4e8373a9ad189c90be69081c2
ZQingda <qingda.zou@gmail.com>
2018-11-24 15:31:59 -0500

    client update 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,71 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 //import logo from './logo.svg';
-//import './App.css';
+import './App.css';
+
+import LineGraph from './components/LineGraph';
+import NodeBox from './components/NodeBox';
 
 class App extends Component {
 
   state = {
-    temperatureData: [],
+    nodes: [],
+    displayGraph: -1,
+    count: 0
   };
 
   componentDidMount() {
-    this.getDataFromDb();
+    var intervalId = setInterval(this.getDataFromDb, 1000);
+    //this.getDataFromDb();
+    this.setState({intervalId : intervalId});
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
   }
 
   getDataFromDb = () => {
-    fetch("/api/getData")
+    this.setState({count: this.state.count + 1});
+    //console.log("GET STATE : " + this.state.count);
+    fetch("http://localhost:3001/api/getData")
       .then(data => data.json())
-      .then(res => this.setState({ temperatureData: res.tempData }));
+      .then(res => {
+        this.setState({ nodes: res.data })
+        //console.log(res.data);
+      });
   };
 
+  setGraphDisplay(graphIndex) {
+    this.setState({displayGraph: graphIndex});
+    console.log(this.state.displayGraph);
+  }
+
   render() {
+    const nodes = this.state.nodes;
+    console.log(nodes);
+
+    var graphDisplay = (
+      <div id="graph-container" style={{opacity: this.state.displayGraph == -1 ? 0 : 100, visibility: this.state.displayGraph == -1 ? "hidden" : "visible"}}>
+        <i onClick={() => {this.setGraphDisplay(-1)}} class="far fa-times-circle"></i>
+        <LineGraph color="#8884d8" field="temperature" data={nodes[this.state.displayGraph] ? nodes[this.state.displayGraph] : {}}/>
+        <LineGraph color="#1abc9c" field="sound" data={nodes[this.state.displayGraph] ? nodes[this.state.displayGraph] : {}}/>
+        <LineGraph color="#f4d03f" field="light" data={nodes[this.state.displayGraph] ? nodes[this.state.displayGraph] : {}}/>
+      </div>
+    )
+
     return (
       <div className="App">
         <header className="App-header">
           {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>Event Activity Heatmap</h1>
         </header>
+        <div className="node-container">
+          {nodes.map((node, i) => {
+            return <NodeBox handleClick={this.setGraphDisplay.bind(this)} data={node} key={i} graphIndex={i}/>;
+          })}
+        </div>
+        {graphDisplay}
       </div>
     );
   }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const cors = require("cors");
 //const Data = require("./data");
 
 const API_PORT = 3001;
 const app = express();
 const router = express.Router();
+
+app.use(cors());
+app.options('*', cors());
 
 // (optional) only made for logging and
 // bodyParser, parses the request body to be a readable json format
@@ -13,10 +17,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-var tempInfo = [15, 16, 16, 18, 25, 25, 16];
+var data = [
+  {
+    nodeName: "Room A400",
+    nodeData:
+    [
+      {name: '1121', temperature: 4000, sound: 2400, light: 2400},
+      {name: '1122', temperature: 3000, sound: 1398, light: 2210},
+      {name: '1123', temperature: 2000, sound: 9800, light: 2290},
+      {name: '1124', temperature: 2780, sound: 3908, light: 2000},
+      {name: '1125', temperature: 1890, sound: 4800, light: 2181},
+      {name: '1126', temperature: 2390, sound: 3800, light: 2500},
+      {name: '1127', temperature: 3490, sound: 4300, light: 2100},
+    ]
+  },
+  {
+    nodeName: "Room B2",
+    nodeData:
+    [
+      {name: '1121', temperature: 25, sound: 500, light: 500},
+      {name: '1122', temperature: 26, sound: 550, light: 550},
+      {name: '1123', temperature: 26, sound: 590, light: 600},
+      {name: '1124', temperature: 27, sound: 570, light: 500},
+      {name: '1125', temperature: 26, sound: 560, light: 480},
+      {name: '1126', temperature: 28, sound: 550, light: 488},
+      {name: '1127', temperature: 29, sound: 600, light: 489},
+    ]
+  }
+];
 
 router.get("/getData", (req, res) => {
-  return res.json({tempData: tempInfo, name: "Temperature information"});
+  console.log(data);
+  return res.json({data: data});
 });
 
 // append /api for our http requests
