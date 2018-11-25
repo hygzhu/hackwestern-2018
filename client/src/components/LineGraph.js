422d85e595c8824f84a7e759329785fff0345272
RunEMC <runlin.li@gmail.com>
2018-11-24 20:39:58 -0500

    qinchang

--- client/src/components/LineGraph.js
+++ client/src/components/LineGraph.js
@@ -3,6 +3,21 @@ import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'rec
 
 class LineGraph extends Component {
 
+  state = {
+    temperature: {
+      min: 16,
+      max: 42
+    },
+    sound: {
+      min: 33,
+      max: 47
+    },
+    light: {
+      min: 0,
+      max: 105
+    }
+  };
+
   render () {
 
     var roundedData = this.props.data.nodeData;
@@ -12,7 +27,7 @@ class LineGraph extends Component {
         <LineChart width={550} height={140} data={this.props.data.nodeData}>
         <CartesianGrid strokeDasharray="3 3"/>
         <XAxis dataKey="name" padding={{left: 30, right: 30}}/>
-        <YAxis/>
+        <YAxis domain={[state[this.props.field].min, state[this.props.field].max]}/>
         <Tooltip/>
         <Legend />
         <Line isAnimationActive={false} type="monotone" dataKey={this.props.field} stroke={this.props.color} activeDot={{r: 3}}/>
